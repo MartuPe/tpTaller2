1f2c7acde24593ad5cb3e2f91a75ea3d14b69a9c
Mora Castelli G <mcastelligaraycochea314@alumno.unlam.edu.ar>
2024-11-06 13:56:49 -0300

    Algo del back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Tarea {
+  id: number;
+  titulo: string;
+  descripcion: string;
+  completada: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tpTaller2';
+export class AppComponent implements OnInit {
+  nombre = 'A-nota-2';
+  tareas: Tarea[] = [];
 
+  ngOnInit() {
+    this.listarTareas();
+  }
+
+  listarTareas() {
+    // llamaria a la api?
+    this.tareas = [
+      { id: 1, titulo: 'Tarea 1', descripcion: 'Descripción de la tarea 1', completada: false },
+      { id: 2, titulo: 'Tarea 2', descripcion: 'Descripción de la tarea 2', completada: false },
+    ];
+  }
+
+  crearTarea(titulo: string, descripcion: string) {
+    const newTarea: Tarea = { id: this.tareas.length + 1, titulo, descripcion, completada: false };
+    this.tareas.push(newTarea);
+  }
+
+  eliminarTarea(id: number) {
+    this.tareas = this.tareas.filter(tareas => tareas.id !== id);
+  }
+
+  modificarTarea(id: number) {
+    this.tareas = this.tareas.filter(tareas => tareas.id !== id);
+  }
+
+  toggleCompletada(tarea: Tarea) {
+    tarea.completada = !tarea.completada;
+  }
 }
